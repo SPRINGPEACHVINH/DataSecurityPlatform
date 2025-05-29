76a104b6ee721a4eb36be7b84d937cfd7847c85a
SPINGPEACHVINH <daoxuan.vinh3@gmail.com>
2025-05-29 13:46:04 +0700

    Update Authentication for Purview API

--- server/routes/purview.js
+++ server/routes/purview.js
@@ -1,11 +1,12 @@
 // Endpoints that Microsoft Purview will call
 import express from 'express';
 import { getOAuth2Token, handleCreateRule, handleRunScan } from '../controllers/purviewEventController.js';
+import { isAuthenticated } from '../middleware/isAuthenticated.js';
 
 const router = express.Router();
 
-router.get('/oauth2', getOAuth2Token);
-router.put('/create-classification-rules', handleCreateRule);
-router.post('/run-scan', handleRunScan);
+router.get('/oauth2', isAuthenticated, getOAuth2Token);
+router.put('/create-classification-rules', isAuthenticated, handleCreateRule);
+router.post('/run-scan',isAuthenticated, handleRunScan);
 
 export default router;
